2e03e712f16d9fb94483609233b162a44e63dd1a
catsriel <danbro07@imkatta.no>
2025-03-07 15:13:37 +0100

    Do testing
    Do a bit of testing around. Minor modification of the code.

--- logic/react.js
+++ logic/react.js
@@ -1,7 +1,14 @@
 const app = document.getElementById("app");
 
+function Header(props){
+    console.log(props.title);
+    return <h1>This is a thing</h1>;
+}
+
 function Top(){
-    return <h1>test</h1>;
+    return (
+        <Header title="HeaderTitle"/>
+    )
 }
 
 const root = ReactDOM.createRoot(app);
