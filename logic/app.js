4faf3088269979a4f498fa68ad9685fc732f908e
Vultus Limunaria <danobrovold@gmail.com>
2025-03-10 16:05:03 +0100

    Update app.js

--- logic/app.js
+++ logic/app.js
@@ -1,3 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { createRoot } from "react-dom/client";
+
+function app(){
+    return (
+        <h1>test</h1>
+    )
+}
+
+const container = document.getElementById("app");
+const root = createRoot(container);
+root.render(app);
