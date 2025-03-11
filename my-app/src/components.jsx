e9eefbd53331cebd8c33ca10857e9f2d0c5a4ef2
MementoM0Rl <Braskerud08@gmail.com>
2025-03-11 09:30:50 +0100

    Update README
    My name is david, dad, i want some icecream, david. that is my name, david, where is my ball....im running out on the road, there is a car, and itd going to hit me ahhhhhhh

--- my-app/src/components.jsx
+++ my-app/src/components.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 function OpeningText(){
-    return <h1>Hello World</h1>
+    return (
+    <div>
+        <h1>Pizza</h1>
+    </div>
+)
 }
 
 export default OpeningText;
